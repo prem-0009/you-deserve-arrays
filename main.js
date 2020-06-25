d0e1b432a849e21cccd50ba9ff15cc8d897a2cc6
prem <prem.sherpa@codeimmersives.com>
2020-06-25 15:30:34 -0400

    do it again arrays?

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function getFirstItemFrom(arr){
+  return arr[0];
+}
 
+function getLastItemFrom(arr){
+  return arr[arr.length-1];
+}
+//confusing
+function getIndex3(arr){
+  if(arr.length>=4){
+    return arr[3];
+  } else {
+    return arr[arr.length-1]
+  }
+  
+}
 
+function isLongList(arr){
+  return arr.length >=10;
+}
 
+function firstItemIsNumber(arr){
+  return Number.isInteger(arr[0])===true;
+}
 
+function secondCharOfThirdString(arr){
+  return arr[2][1];
+}
 
 
 
